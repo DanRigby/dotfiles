b373f217162cb3230264f4f7a3be0245d85b3a09
Dan Rigby <danrigby@github.com>
2020-04-23 22:17:02 -0400

    Need to run abbr from fish

--- install.sh
+++ install.sh
@@ -11,30 +11,13 @@ echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/ins
 echo "eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> /workspace/.bash_profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# Install misc tools
+# Install tools
 brew install fish
 brew install bat
 brew install fd
 brew install hyperfine
 brew install diskus
 brew install exa
-
-# Setup fish
-abbr -a cbuild '/workspace/enterprise2/chroot-build.sh'
-abbr -a creset '/workspace/enterprise2/chroot-reset.sh'
-abbr -a cconfig '/workspace/enterprise2/chroot-configure.sh'
-abbr -a cstart '/workspace/enterprise2/chroot-start.sh'
-abbr -a cinfo '/workspace/enterprise2/chroot-info.sh'
-abbr -a cssh '/workspace/enterprise2/chroot-ssh.sh'
-abbr -a cip '/workspace/enterprise2/chroot-ip.sh'
-abbr -a cstop '/workspace/enterprise2/chroot-stop.sh'
-abbr -a ccinfo '/workspace/enterprise2/chroot-cluster-info.sh'
-abbr -a ccip '/workspace/enterprise2/chroot-cluster-ip.sh'
-abbr -a ccstart '/workspace/enterprise2/chroot-cluster-start.sh'
-abbr -a ccstop '/workspace/enterprise2/chroot-cluster-stop.sh'
-abbr -a ccssh '/workspace/enterprise2/chroot-cluster-ssh.sh'
-abbr -a gclean 'sudo git clean -fdx'
-abbr -a dclean 'docker system prune --all'
 
 # Configure git
 git config --global user.name 'Dan Rigby'
@@ -54,3 +37,22 @@ git config --global alias.agc 'gc --aggressive --prune=all'
 git config --global help.autocorrect '30'
 git config --global alias.lg 'log --color --graph --pretty=format"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 git config --global alias.up '!git remote update -p; git merge --ff-only @{u}; git submodule update --init --recursive'
+
+# Configure fish shell
+fish
+abbr -a cbuild '/workspace/enterprise2/chroot-build.sh'
+abbr -a creset '/workspace/enterprise2/chroot-reset.sh'
+abbr -a cconfig '/workspace/enterprise2/chroot-configure.sh'
+abbr -a cstart '/workspace/enterprise2/chroot-start.sh'
+abbr -a cinfo '/workspace/enterprise2/chroot-info.sh'
+abbr -a cssh '/workspace/enterprise2/chroot-ssh.sh'
+abbr -a cip '/workspace/enterprise2/chroot-ip.sh'
+abbr -a cstop '/workspace/enterprise2/chroot-stop.sh'
+abbr -a ccinfo '/workspace/enterprise2/chroot-cluster-info.sh'
+abbr -a ccip '/workspace/enterprise2/chroot-cluster-ip.sh'
+abbr -a ccstart '/workspace/enterprise2/chroot-cluster-start.sh'
+abbr -a ccstop '/workspace/enterprise2/chroot-cluster-stop.sh'
+abbr -a ccssh '/workspace/enterprise2/chroot-cluster-ssh.sh'
+abbr -a gclean 'sudo git clean -fdx'
+abbr -a dclean 'docker system prune --all'
+exit
